fbc99948bc25dbdffc41eee6d115d82c4b17f644
Arthur Palacio <ruhtradev10@gmail.com>
2026-08-31 15:23:00 -0300

    feat: criar estrutura visual principal (#4)

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
+import MainLayout from './layouts/MainLayout/MainLayout'
+
 export default function App() {
   return (
-    <></>
+    <MainLayout>
+      <p>Conteúdo principal</p>
+    </MainLayout>
   )
 }
