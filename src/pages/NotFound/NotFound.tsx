90267432d53bbf0972840d9f6e8f1a2377650d36
Felipe Lima <148886103+felipelima2005@users.noreply.github.com>
2026-09-07 19:36:04 -0300

    Update src/pages/NotFound/NotFound.tsx
    fix: Alteração na frase  de página não encontrada
    
    Co-authored-by: Murilo de Souza <161900046+murilo-a-souza@users.noreply.github.com>

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -4,7 +4,7 @@ export default function NotFound() {
     return (
         <main>
             <h1>Erro 404</h1>
-            <h3>O endereço da página solicitada não foi encontrada</h3>
+            <h3>O endereço da página solicitada não foi encontrado</h3>
             <nav>
                 <button><Link to ="/">HOME</Link></button>
             </nav>
