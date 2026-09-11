8cee8e6fdc3b2df477ff1bc3389988dd6f742c4b
carlostsucamoto <caduchiarelli@hotmail.com>
2026-09-10 21:16:55 -0300

    feat: instalação e configuração do tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,13 @@ import AppRoutes from './routes/AppRoutes'
 
 export default function App() {
   return (
-    <MainLayout>
-      <AppRoutes />
-    </MainLayout>
+    <>
+      <div className="text-3xl font-bold text-blue-600 p-4">
+        Seja bem-vindo
+      </div>
+      <MainLayout>
+        <AppRoutes />
+      </MainLayout>
+    </>
   )
 }
