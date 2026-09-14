51007591f4197e234b56efa006bb6905e6fac8a0
I.neeto99 <italo.neeto.oliv@outlook.com>
2026-09-14 20:22:19 -0300

    refactor: modela recursos de acessibilidade como union de literais

--- src/types/local.ts
+++ src/types/local.ts
@@ -1,19 +1,29 @@
+export type Categoria =
+  | 'restaurante'
+  | 'saude'
+  | 'educacao'
+  | 'lazer'
+  | 'servico_publico';
+
+export type RecursoAcessibilidade =
+  | 'rampa_acesso'
+  | 'banheiro_adaptado'
+  | 'piso_tatil'
+  | 'sinalizacao_visual'
+  | 'vagas_estacionamento'
+  | 'braile'
+  | 'libras'
+  | 'elevador'
+  | 'balcao_acessivel'
+  | 'assentos_prioritarios'
+  | 'espaco_tranquilo'
+  | 'cao_guia';
+
 export type Local = {
   id: number;
   nome: string;
   endereco: string;
   cep: string;
   categoria: Categoria;
-  recursosAcessibilidade: RecursosAcessibilidade;
-};
-
-export type Categoria = 'restaurante' | 'saude' | 'educacao' | 'lazer' | 'servico_publico';
-
-export type RecursosAcessibilidade = {
-  rampaAcesso: boolean;
-  banheiroAdaptado: boolean;
-  pisoTatil: boolean;
-  sinalizacaoVisual: boolean;
-  vagasEstacionamento: boolean;
-  braile: boolean;
+  recursosAcessibilidade: RecursoAcessibilidade[];
 };
